f7475fb34180a039d58225c16bb44acfe8ca5e07
Renzo Davila <rdavilac@emeal.nttdata.com>
2022-02-07 19:07:28 -0500

    agregando firebase

--- angular-firebase/src/environments/environment.ts
+++ angular-firebase/src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'angfirebase2022-rd',
+    appId: '1:163127415021:web:0e74d9798f71a23531fce4',
+    storageBucket: 'angfirebase2022-rd.appspot.com',
+    apiKey: 'AIzaSyCDSWToSQ2DFmYXXDiYu5B-c4jeuyifKPs',
+    authDomain: 'angfirebase2022-rd.firebaseapp.com',
+    messagingSenderId: '163127415021',
+    measurementId: 'G-J3HE9T9NH2',
+  },
   production: false
 };
 
